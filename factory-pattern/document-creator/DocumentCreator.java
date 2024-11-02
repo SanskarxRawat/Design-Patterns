b5d598b1879afe88bdc04d4f082d573b5c8f85d5
Sanskar Rawat <sanskar@credochain.com>
2024-11-02 17:23:26 +0530

    "feat:desc added doc factory commit"

--- factory-pattern/document-creator/DocumentCreator.java
+++ factory-pattern/document-creator/DocumentCreator.java
@@ -1,5 +1,12 @@
 public class DocumentCreator {
 
+    /**
+     * Explanation:
+     * 1. The `Document` interface acts as a common product interface for all document types.
+     * 2. `PDFDocument`, `WordDocument`, and `HTMLDocument` are concrete implementations of the `Document` interface.
+     * 3. The `DocumentFactory` is a factory class that is responsible for creating instances of different document types based on the user input.
+     * 4. The `DocumentCreator` class is the client that interacts with the user and utilizes the factory to create documents.
+     */
     public static void main(String[] args) {
 
         Document htmlDocument=DocumentFactory.createDocument("html");
